4c84ff88a969fb7a8205a43225bfea02eee07b6f
Samuel Marques <samuel.s.marques@protonmail.com>
2023-10-17 22:27:05 -0300

    adiciona pesquisa

--- src/naEsquinaDeCasa/Main.java
+++ src/naEsquinaDeCasa/Main.java
@@ -1,6 +1,7 @@
 package naEsquinaDeCasa;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +9,7 @@ public class Main {
 		ArrayList<Cliente> listaClientes = new ArrayList<>();
 		Scanner scanner = new Scanner(System.in);
 		boolean continuar = true;
+		ArvoreAVL arvore = new ArvoreAVL();
 
 		while (continuar) {
 			System.out.print("Digite o nome completo do cliente: ");
@@ -29,6 +31,7 @@ public class Main {
 			String cpf = scanner.nextLine();
 
 			Cliente cliente = new Cliente(nomeCompleto, dataNascimento, contatoTelefonico, email, endereco, cpf);
+			arvore.inserir(cliente);
 			listaClientes.add(cliente);
 
 			System.out
@@ -38,11 +41,27 @@ public class Main {
 			if (resposta.equalsIgnoreCase("n")) {
 				continuar = false;
 			} else if (resposta.equalsIgnoreCase("p")) {
-				realizarPesquisa(listaClientes, scanner);
+				realizarPesquisa(listaClientes, scanner, arvore);
+
+				System.out.println("Deseja continuar cadastrando ou cancelar? (S para continuar, N para cancelar): ");
+				String respostaPesquisa = scanner.nextLine();
+				if (respostaPesquisa.equalsIgnoreCase("n")) {
+					continuar = false;
+				}
 			}
 		}
 
 		scanner.close();
+
+		List<Cliente> clientesOrdenados = ordenarRegistrosPorNome(listaClientes);
+
+		for (Cliente cliente : clientesOrdenados) {
+			System.out.println(cliente.getNomeCompleto());
+		}
+	}
+
+	public static void entrada() {
+
 	}
 
 	public static Cliente buscarPorCpf(ArrayList<Cliente> listaClientes, String cpf) {
@@ -65,10 +84,62 @@ public class Main {
 		return clientesEncontrados;
 	}
 
-	public static void realizarPesquisa(ArrayList<Cliente> listaClientes, Scanner scanner) {
+	public static Cliente buscarPorCpfAVL(No raiz, String cpf) {
+		if (raiz == null) {
+			return null;
+		}
+
+		if (raiz.cliente.getCpf().startsWith(cpf)) {
+			return raiz.cliente;
+		} else if (cpf.compareTo(raiz.cliente.getCpf()) < 0) {
+			return buscarPorCpfAVL(raiz.esquerda, cpf);
+		} else {
+			return buscarPorCpfAVL(raiz.direita, cpf);
+		}
+	}
+
+	public static List<String> gerarListaCPFsEmOrdem(No raiz) {
+		List<String> listaCPFs = new ArrayList<>();
+		gerarListaCPFsEmOrdemAux(raiz, listaCPFs);
+		return listaCPFs;
+	}
+
+	private static void gerarListaCPFsEmOrdemAux(No no, List<String> listaCPFs) {
+		if (no != null) {
+			gerarListaCPFsEmOrdemAux(no.esquerda, listaCPFs);
+			listaCPFs.add(no.cliente.getCpf());
+			gerarListaCPFsEmOrdemAux(no.direita, listaCPFs);
+		}
+	}
+
+	public static List<Cliente> ordenarRegistrosPorNome(List<Cliente> clientes) {
+		List<Cliente> listaOrdenada = new ArrayList<>(clientes);
+
+		int n = listaOrdenada.size();
+
+		for (int i = 0; i < n - 1; i++) {
+			int minIndex = i;
+
+			for (int j = i + 1; j < n; j++) {
+				if (listaOrdenada.get(j).getNomeCompleto()
+						.compareTo(listaOrdenada.get(minIndex).getNomeCompleto()) < 0) {
+					minIndex = j;
+				}
+			}
+
+			Cliente temp = listaOrdenada.get(minIndex);
+			listaOrdenada.set(minIndex, listaOrdenada.get(i));
+			listaOrdenada.set(i, temp);
+		}
+
+		return listaOrdenada;
+	}
+
+	public static void realizarPesquisa(ArrayList<Cliente> listaClientes, Scanner scanner, ArvoreAVL arvore) {
 		System.out.println("Escolha a opção de pesquisa: ");
 		System.out.println("1. Pesquisar por CPF");
 		System.out.println("2. Pesquisar por nome");
+		System.out.println("3. Listar CPFs em ordem numérica crescente");
 		int opcao = scanner.nextInt();
 		scanner.nextLine();
 
@@ -94,6 +165,23 @@ public class Main {
 				}
 			} else {
 				System.out.println("Nenhum cliente encontrado por nome.");
+			}
+			break;
+		case 3:
+			System.out.println("Digite o CPF para pesquisa: ");
+			String cpfAvl = scanner.nextLine();
+			Cliente clienteEncontradoAvl = buscarPorCpfAVL(arvore.raiz, cpfAvl);
+
+			if (clienteEncontradoAvl != null) {
+				System.out.println("Cliente encontrado: " + clienteEncontradoAvl.getNomeCompleto());
+				List<String> listaCPFsEmOrdem = gerarListaCPFsEmOrdem(arvore.raiz);
+				System.out.println("Número de comparações: " + listaCPFsEmOrdem.size());
+
+				for (String cpfOrdenado : listaCPFsEmOrdem) {
+					System.out.println("CPF: " + cpfOrdenado);
+				}
+			} else {
+				System.out.println("Cliente não encontrado.");
 			}
 			break;
 		default:
